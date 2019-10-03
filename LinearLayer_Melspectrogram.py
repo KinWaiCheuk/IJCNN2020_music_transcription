06117bab55b1408ee89286470775cd1877f74094
Raven Cheuk <u3500684@connect.hku.hk>
2019-10-03 12:27:05 +0800

    Add experiment files

---
+++ LinearLayer_Melspectrogram.py
@@ -0,0 +1,276 @@
+import os,sys,signal
+import math
+
+import pickle
+import numpy as np                                       # fast vectors and matrices
+import matplotlib.pyplot as plt                          # plotting
+sys.path.insert(0, '../')
+
+import musicnet
+
+from time import time
+
+from sklearn.metrics import average_precision_score
+
+os.environ['CUDA_DEVICE_ORDER']='PCI_BUS_ID'   # see issue #152
+
+import torch
+from torch.nn.functional import conv1d, mse_loss
+import torch.nn.functional as F
+import torch.nn as nn
+
+import Spectrogram
+import argparse
+
+    
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--GPU", help="Choose which GPU to use", type=str)
+parser.add_argument("-e", "--epochs", help="Set num epochs", type=int)
+parser.add_argument("-w", "--window_size", help="Set input audio window size", type=int)
+parser.add_argument("-m", "--n_mels", help="Set the number of mel bins", type=int)
+parser.add_argument("--htk", help="Rather to use htk mel scale or not", type=int)
+parser.add_argument("-c", "--center", help="set whether center the window or not", type=bool)
+# Since CQT only supports center=True, I think other experiments also need to set center=True
+
+args = parser.parse_args()
+if args.GPU:
+    os.environ['CUDA_VISIBLE_DEVICES']=args.GPU
+else:
+    os.environ['CUDA_VISIBLE_DEVICES']='0'
+    
+if torch.cuda.is_available():
+    device = "cuda:0"
+    torch.set_default_tensor_type('torch.cuda.FloatTensor')    
+    
+# Network Parameters    
+if args.epochs:
+    epochs = args.epochs   
+else:
+    epochs = 35
+
+train_size = 100000
+test_size = 50000
+epsilon = 1e-5
+fs = 44100
+
+lr = 1e-6
+momentum = .95
+
+pitch_shift = 0
+jitter = 0.
+num_workers = 10
+sequence = 1
+
+# lvl1 convolutions are shared between regions
+m = 128
+k = 512              # lvl1 nodes
+n_fft = 4096              # lvl1 receptive field
+
+if args.window_size:
+    window = args.window_size
+else:
+    window = 16384 # total number of audio samples?
+stride = 512
+batch_size = 100
+
+filename = os.path.splitext(__file__)[0]
+
+if args.center:
+    center = args.center
+else:
+    center = True
+
+if center:
+    regions = 1 + (window)//stride
+else:
+    regions = 1 + (window - n_fft)//stride
+
+if args.htk:
+    htk = True
+    htk_mode = 'htk'
+    print(type(args.htk))
+    print(htk)
+    
+else:
+    htk = False
+    htk_mode = 'quasi'
+    print(type(args.htk))
+    print(htk)
+if args.n_mels:
+    n_mels = args.n_mels
+else:
+    n_mels=256
+
+def worker_init(args):
+    signal.signal(signal.SIGINT, signal.SIG_IGN) # ignore signals so parent can handle them
+    np.random.seed(os.getpid() ^ int(time())) # approximately random seed for workers
+kwargs = {'num_workers': num_workers, 'pin_memory': True, 'worker_init_fn': worker_init}
+
+# function for returning scientific notation in a plot
+def fmt(x, pos):
+    a, b = '{:.0e}'.format(x).split('e')
+    b = int(b)
+    return fr'${a} \times 10^{{{b}}}$'
+
+result_dict = {'loss_history_train': [],
+               'avgp_history_train': [],
+               'loss_history_test': [],
+               'avgp_history_test': [],
+               "Mir_Eval": [],
+               'parameters': {}}
+
+result_dict['parameters']['train_size'] = train_size
+result_dict['parameters']['test_size'] = test_size
+result_dict['parameters']['lr'] = lr
+result_dict['parameters']['pitch_shift'] = pitch_shift
+result_dict['parameters']['jitter'] = jitter
+result_dict['parameters']['window'] = window
+
+# Preparing Dataset
+
+start = time()
+root = './data/'
+train_set = musicnet.MusicNet(root=root, epoch_size=train_size,sequence=sequence
+                              , train=True, download=True, refresh_cache=False, 
+                              window=window, mmap=False, pitch_shift=pitch_shift, jitter=jitter)
+test_set = musicnet.MusicNet(root=root, train=False, download=True,sequence=sequence
+                             , refresh_cache=False, window=window, epoch_size=test_size, mmap=False)
+print("Data loaded, time used = {:2.2f} seconds".format(time()-start))
+
+train_loader = torch.utils.data.DataLoader(dataset=train_set,batch_size=batch_size,**kwargs)
+test_loader = torch.utils.data.DataLoader(dataset=test_set,batch_size=batch_size,**kwargs)
+
+# Defining Models
+
+Loss = torch.nn.BCELoss()
+def L(yhatvar,y):
+    return Loss(yhatvar,y) * 128/2
+
+class Model(torch.nn.Module):
+    def __init__(self, avg=.9998):
+        super(Model, self).__init__()
+        # Getting Mel Spectrogram on the fly
+        self.mel_layer = Spectrogram.MelSpectrogram(sr=fs, n_fft=n_fft, n_mels=n_mels, htk=htk, fmin=50, fmax=6000, center=center)
+            
+        # Creating Layers
+        self.linear = torch.nn.Linear(n_mels*regions, m, bias=False)
+        torch.nn.init.constant_(self.linear.weight, 0) # initialize
+        
+        self.avg = avg
+        
+    def forward(self,x):
+        z = self.mel_layer(x)
+        y = self.linear((torch.log(z+epsilon)).view(x.data.size()[0],n_mels*regions))
+        return torch.sigmoid(y)
+    
+
+model = Model()
+model.to(device)
+
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+# optimizer = SWA(base_opt, swa_start=0, swa_freq=1, swa_lr=0.000001)
+
+try:
+    with train_set, test_set:
+        print("epoch\ttrain loss\ttest loss\ttrain avg\ttest avg\ttime\tutime")
+        for e in range(epochs):
+            yground = torch.Tensor(batch_size*len(train_loader), m) # what not do this together with loss
+            yhat = torch.Tensor(batch_size*len(train_loader), m)
+            avgp, loss_e = 0.,0
+            t = time()
+            for i, (x,y) in enumerate(train_loader):
+                print(f"training {i}/{len(train_loader)} batches", end = '\r')
+                optimizer.zero_grad()
+                
+                # making x and y into pytorch dealable format
+                x = x.to(device, non_blocking=True)
+                y = y.to(device, non_blocking=True)
+                yhatvar = model(x)
+                loss = L(yhatvar,y)
+                loss.backward()
+                loss_e += loss.item() #getting the number
+                
+                yground[i*batch_size:(i+1)*batch_size] = y.data
+                yhat[i*batch_size:(i+1)*batch_size] = yhatvar.data
+                
+                optimizer.step()
+            avgp = average_precision_score(yground.cpu().flatten(),yhat.cpu().flatten())  
+            
+            result_dict['loss_history_train'].append(loss_e/len(train_loader))
+            result_dict['avgp_history_train'].append(avgp)   
+            t1 = time()
+            avgp, loss_e = 0.,0.           
+#             optimizer.swap_swa_sgd() # change to average weight
+            
+            # For testing
+            yground = torch.Tensor(batch_size*len(test_loader), m) # what not do this together with loss
+            yhat = torch.Tensor(batch_size*len(test_loader), m)
+            
+            for i, (x_test,y_test) in enumerate(test_loader):
+                print(f"testing {i}/{len(test_loader)} batches", end = '\r')
+                x_test = x_test.to(device)
+                y_test = y_test.to(device)
+                yhatvar = model(x_test)
+                loss_e += L(yhatvar, y_test).item() #getting the number
+
+                yground[i*batch_size:(i+1)*batch_size] = y_test.data
+                yhat[i*batch_size:(i+1)*batch_size] = yhatvar.data
+            avgp = average_precision_score(yground.cpu().flatten(),yhat.cpu().flatten())
+            result_dict['loss_history_test'].append(loss_e/len(test_loader))
+            result_dict['avgp_history_test'].append(avgp)
+            print(' '*50, end='\r') # Clearing previous content
+            print('{}\t{:2f}\t{:2f}\t{:2f}\t{:2f}\t{:2.1f}\t{:2.1f}'.                  format(e,
+                         result_dict['loss_history_train'][-1],result_dict['loss_history_test'][-1],
+                         result_dict['avgp_history_train'][-1],result_dict['avgp_history_test'][-1],
+                         time()-t, time()-t1))
+
+
+        
+except KeyboardInterrupt:
+    print('Graceful Exit')
+else:
+    print("Finsihed")
+    
+
+fig, ax = plt.subplots(1, 2, figsize=(10,4))
+
+ax[0].plot(result_dict['loss_history_train'])
+ax[0].plot(result_dict['loss_history_test'])
+ax[0].legend(['train', 'test'])
+ax[0].set_title('Loss', size=20)
+
+ax[1].plot(result_dict['avgp_history_train'])
+ax[1].plot(result_dict['avgp_history_test'])
+ax[1].legend(['train', 'test'])
+ax[1].set_title('Accuracy', size=20)
+ax[1].set_ylim(0.3,0.80)
+
+# Mir_Eval stats
+
+print('AvgP\tP\tR\tAcc\tETot\tESub\tEmiss\tEfa')
+Accavg = 0
+Etotavg = 0
+model.eval()
+for songid in test_set.rec_ids:
+    Y_pred, Y_true = musicnet.get_piano_roll(songid, test_set, model, device,
+                                             window=window, m=m, stride=-1)
+    _,_,_,Acc,Etot = musicnet.get_mir_accuracy(Y_pred, Y_true, m=m)
+    Accavg += Acc
+    Etotavg += Etot
+    result_dict['Mir_Eval'].append([Acc, Etot])
+
+print('Average Accuracy: \t{:2.2f}\nAverage Error: \t\t{:2.2f}'
+      .format(Accavg/len(test_set.rec_ids)*100, Etotavg/len(test_set.rec_ids)*100))
+
+# Saving weights and results
+if center:
+    torch.save(model.state_dict(), './weights/'+filename+ '_e-{}_w-{}_mbins-{}_{}_center'.format(epochs, window, n_mels, htk_mode))
+    with open('./result_dict/'+filename+ '_e-{}_w-{}_mbins-{}_{}_center'.format(epochs, window, n_mels, htk_mode), 'wb') as f:
+        pickle.dump(result_dict, f)
+else:
+    torch.save(model.state_dict(), './weights/'+filename+ '_e-{}_w-{}_mbins-{}_{}'.format(epochs, window, n_mels, htk_mode))
+    with open('./result_dict/'+filename+ '_e-{}_w-{}_mbins-{}_{}'.format(epochs, window, n_mels, htk_mode), 'wb') as f:
+        pickle.dump(result_dict, f)   
+
+
+
---
+++ cqt_exp.sh
@@ -0,0 +1,7 @@
+#!/opt/conda/bin/python
+python LinearLayer_CQT.py -r 6 -g 3
+python LinearLayer_CQT.py -r 7 -g 3
+python LinearLayer_CQT.py -r 8 -g 3
+python LinearLayer_CQT.py -r 9 -g 3
+python LinearLayer_CQT.py -r 10 -g 3
+
